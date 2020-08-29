8b82b36f763dbb72118b8b5a121be14c7d780006
gokul2000 <ngokulnarayanan@gmail.com>
2020-08-29 19:21:56 +0530

    Added StartUpProgram

---
+++ src/com/company/Basics/StartUpProgram.java
@@ -0,0 +1,14 @@
+/*
+ * Created by Gokul on $today.date/ $toay.month/2020
+ */
+
+package com.company.Basics;
+
+/**
+ * Created by Gokul on Aug,2020,29-08-2020 at 19:19
+ */
+public class StartUpProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
