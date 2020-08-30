cc5b509d2b13d84c8e32807901b7f971d1ddf8d4
gokul2000 <ngokulnarayanan@gmail.com>
2020-08-30 17:10:04 +0530

    Serialization example is added

---
+++ src/com/company/Basics/ProgramOne.java
@@ -0,0 +1,31 @@
+/*
+ * Created by Gokul on $today.date/ $toay.month/2020
+ */
+
+package com.company.Basics;
+
+import java.util.Scanner;
+
+/**
+ * Created by Gokul on Aug,2020,30-08-2020 at 16:38
+ */
+public class ProgramOne {
+    public static String str="Gokul";
+
+    public static void main(String[] args) {
+        int a;
+        String b;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        sc.nextLine();
+        b=sc.nextLine();
+        System.out.println(a+" "+b);
+
+
+
+
+    }
+
+
+}
+
