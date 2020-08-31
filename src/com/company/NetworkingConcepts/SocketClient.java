26776e3f13ced589a38c8c14af4434f7d547b9f1
gokul2000 <ngokulnarayanan@gmail.com>
2020-08-31 17:23:43 +0530

    Networking related Programs

---
+++ src/com/company/NetworkingConcepts/SocketClient.java
@@ -0,0 +1,26 @@
+/*
+ * Created by Gokul on $today.date/ $toay.month/2020
+ */
+
+package com.company.NetworkingConcepts;
+
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by Gokul on Aug,2020,31-08-2020 at 17:12
+ */
+public class SocketClient {
+    public static void main(String[] args) throws  Exception {
+        String ip = "localhost";
+        int port=9999;
+        String str="Gokul";
+        Socket s = new Socket(ip,port);
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(s.getOutputStream());
+        PrintWriter printWriter = new PrintWriter(outputStreamWriter);
+        printWriter.println(str);
+        printWriter.flush();
+
+    }
+}
---
+++ src/com/company/NetworkingConcepts/SocketServer.java
@@ -0,0 +1,34 @@
+/*
+ * Created by Gokul on $today.date/ $toay.month/2020
+ */
+
+package com.company.NetworkingConcepts;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.Buffer;
+
+/**
+ * Created by Gokul on Aug,2020,31-08-2020 at 17:12
+ */
+public class SocketServer {
+    public static void main(String[] args)  throws  Exception {
+        System.out.println("Server : Server Started......");
+        ServerSocket serverSocket = new ServerSocket(9999);
+        System.out.println("Server : Server Started Listening");
+        Socket s = serverSocket.accept();
+        System.out.println("Server : Client was Connected");
+        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String msg=br.readLine();
+        System.out.println("Client :"+msg);
+        OutputStreamWriter os = new OutputStreamWriter(s.getOutputStream());
+        PrintWriter printWriter = new PrintWriter(os);
+        printWriter.println("Your message Recieved");
+        printWriter.flush();
+        System.out.println("Server : Data Sent to Client");
+        serverSocket.close();
+
+
+    }
+}
