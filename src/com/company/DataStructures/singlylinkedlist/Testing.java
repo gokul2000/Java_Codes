d88a22e8144b3240aa86c3b81be845dfaf0c1a9a
gokul2000 <ngokulnarayanan@gmail.com>
2020-09-04 18:30:49 +0530

    Data Structures Added

--- src/com/company/DataStructures/singlylinkedlist/Testing.java
+++ src/com/company/DataStructures/singlylinkedlist/Testing.java
@@ -4,18 +4,34 @@
 
 package com.company.DataStructures.singlylinkedlist;
 
+import java.util.Scanner;
+
 /**
  * Created by Gokul on Sep,2020,04-09-2020 at 13:28
  */
 public class Testing {
     public static void main(String[] args) {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
-        singlyLinkedList.insertFirst(5);
-        singlyLinkedList.insert(23);
-        singlyLinkedList.insert(24);
-        singlyLinkedList.insert(25);
-        singlyLinkedList.insert(26);
-        singlyLinkedList.insertFirst(25);
-        singlyLinkedList.printList();
+        Scanner scanner = new Scanner(System.in);
+        boolean cont=true;
+        while (cont){
+            System.out.println("1.Insert....");
+            System.out.println("2.Insert at First...");
+            System.out.println("3.Print List....");
+            System.out.println("Press any other number to exit...");
+            int choice=scanner.nextInt();
+            switch (choice){
+                case 1->{System.out.println("Enter Number");
+                    singlyLinkedList.insert(scanner.nextInt());}
+                case 2->{System.out.println("Enter Number");
+                    singlyLinkedList.insertFirst(scanner.nextInt());}
+                case 3->{
+                    System.out.println("Showing List....");
+                    singlyLinkedList.printList();
+                }
+                default -> {cont=false;}
+            }
+        }
+
     }
 }
