c762ce0f7291ef1f6c112b8abbdcb81feb5721c0
gokul2000 <ngokulnarayanan@gmail.com>
2020-09-07 15:42:30 +0530

    SortedSetImplementation

--- src/com/company/collections/set/SetImplementation2.java
+++ src/com/company/collections/set/SetImplementation2.java
@@ -27,7 +27,7 @@ public class SetImplementation2 {
         System.out.println(sortedSet.stream().max(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                if(o1%10<=02%10){
+                if(o1%10<=o2%10){
                     return -1;
                 }
                 return 1;
