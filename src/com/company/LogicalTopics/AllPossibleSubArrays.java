4119e669de560174194db6854b934adf930350ed
gokul2000 <ngokulnarayanan@gmail.com>
2020-09-08 16:43:11 +0530

    Logical Topics

--- src/com/company/LogicalTopics/AllPossibleSubArrays.java
+++ src/com/company/LogicalTopics/AllPossibleSubArrays.java
@@ -18,18 +18,37 @@ public class AllPossibleSubArrays {
         arr.add(5);
         arr.add(2);
         arr.add(3);
-        for(int b=2;b<=5;b++){
-            for(int i=0;i<=5-b;i++){
+        int b=2;
+        List<String> array = new ArrayList<>();
+        for(int i=0;i<=5-b;i++){
 
-                for(int j=i+1;j<=5-(b-1);j++){
-                    System.out.print(arr.get(i)+" ");
-                    for(int k=j;k<j+(b-1);k++){
-                        System.out.print(arr.get(k)+" ");
-                    }
-                    System.out.println();
+            for(int j=i+1;j<=5-(b-1);j++){
+                String str =(arr.get(i)+"");
+                for(int k=j;k<j+(b-1);k++){
+                    str+=(arr.get(k)+"");
                 }
-
+                array.add(str);
             }
+
+
         }
+        int k=3;
+        while(k<5){
+            for(int i=0;i<array.size();i++){
+                if(array.get(i).length()==k-1){
+                    for(int v=0;v<arr.size();v++){
+                        String s=arr.get(v)+"";
+                        if(!(array.get(i).contains(s))){
+                            String result=array.get(i)+s;
+
+                            array.add(result);
+                        }
+                    }
+                }
+            }
+            k+=1;
+        }
+        System.out.println(array);
     }
-}
+    }
+
